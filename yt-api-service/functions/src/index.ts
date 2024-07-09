9a1e7effef84e75ddbb66e59c7ac712c5a8b149f
leotao <leotao@umich.edu>
2024-07-09 04:48:14 -0400

    feat: made add user firestore function

--- yt-api-service/functions/src/index.ts
+++ yt-api-service/functions/src/index.ts
@@ -1,7 +1,26 @@
-import {onRequest} from "firebase-functions/v2/https";
+import * as functions from "firebase-functions";
+import {initializeApp} from "firebase-admin/app";
+import {Firestore} from "firebase-admin/firestore";
 import * as logger from "firebase-functions/logger";
 
- export const helloWorld = onRequest((request, response) => {
-   logger.info("Hello logs!", {structuredData: true});
-   response.send("Hello from Firebase!");
- });
+initializeApp();
+
+const firestore = new Firestore();
+
+export const createUser = functions.auth.user().onCreate((user) => {
+  // create user functions thats called on event
+  // collect standard userInfo
+  const userInfo = {
+    uid: user.uid,
+    email: user.email,
+    photoUrl: user.photoURL,
+  };
+
+  // Update fireStore with userInfo
+  // If one doesnt exist itll automatically create one
+  firestore.collection("users").doc(user.uid).set(userInfo);
+  // log User Creation
+  logger.info(`User Created: ${JSON.stringify(userInfo)}`);
+  return;
+});
+
